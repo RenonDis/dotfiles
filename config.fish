e196d23be9c3da40ed7810b8c018f39703dfc062
RenonDis <renon@disroot.org>
2018-05-16 13:01:47 +0200

    Added fish config

---
+++ config.fish
@@ -0,0 +1,27 @@
+# Path to Oh My Fish install.
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
+
+# Load Oh My Fish configuration.
+source $OMF_PATH/init.fish
+
+export http_proxy=http://localhost:3128
+export https_proxy=http://localhost:3128
+
+alias vim="nvr -s"
+
+setxkbmap fr
+
+# Go settings
+set -x -U GOPATH $HOME/go
+set -x -U GOBIN $GOPATH/bin
+set PATH /usr/lib/go-1.10/bin $PATH
+set PATH $GOPATH/bin $PATH
+
+#function rgvim
+#    set choice (rg -il $argv | fzf -0 -1 --ansi --preview "cat {} | rg $argv --context 3")
+#    if [ $choice ]
+#        /usr/local/bin/nvim "+/"(to_lower $argv) $choice
+#    end
+#end
---
+++ fish.md
@@ -0,0 +1,5 @@
+# Outils
+
+## Git : visualisation avec GRV
+
+`$ wget -O grv https://github.com/rgburke/grv/releases/download/v0.1.3/grv_v0.1.3_linux64`
